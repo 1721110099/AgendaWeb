a164b1db9a55137ef3394990a1be25cf11127111
1721110099 <97765494+1721110099@users.noreply.github.com>
2022-05-04 15:51:53 +0000

    Web funcionando

---
+++ app.py
@@ -0,0 +1,29 @@
+import web
+import sqlite3
+
+urls = ("/", "personas")
+app = web.application(urls, globals())
+render = web.template.render("views/")
+
+class personas:
+    def GET(self):
+        conection = sqlite3.connect("agenda.db")
+        cursor = conection.cursor()
+        result = cursor.execute("select * from personas")
+        return render.personas(result)
+        
+    def POST(self):
+        form = web.input()
+        conection = sqlite3.connect("agenda.db")
+        cursor = conection.cursor()
+        cursor.execute(
+            "insert into personas(nombre,email) values (?, ?)",
+            (form.nombre, form.email),
+        )
+        
+        conection.commit()
+        conection.close()
+        raise web.seeother("/")
+        
+if __name__ == "__main__":
+    app.run()
